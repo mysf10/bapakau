5b9a4703de870e95abdbc27972b7023d8574df88
Yosh31 <adrianaudie@student.ub.ac.id>
2022-12-10 15:00:56 +0700

    second commit

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -13,7 +13,21 @@
   </head>
   <body>
   <div class="container mt-4">
-    <h1>Ini adalah halaman list buku yang dapat dipinjam</h1>
+
+    {{-- INI BAGIAN YANG DIMODIFIKASI DARI ISI WEBSITE --}}
+  
+    @foreach ($books as $buku)
+
+    <article class="mb-3">
+      <h2>{{ $buku["judul"] }}</h2>
+      <p>{{ $buku["genre"] }}</p>
+      <p>{{ $buku["pengarang"] }}</p>
+      <p>{{ $buku["penerbit"] }}</p>
+      <p>{{ $buku["bahasa"] }}</p>
+    </article>
+        
+    @endforeach
+  
   </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
