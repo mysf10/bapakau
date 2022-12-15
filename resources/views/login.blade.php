68389d0612ca19a7a765bed1960288f95f8d9fbf
Yosh31 <adrianaudie@student.ub.ac.id>
2022-12-15 11:51:20 +0700

    error illuminate

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,20 +10,17 @@
 <body id="loginbox">
   <form action="/login" method="post">
     @csrf
-  <h1>Welcome to Digilab!</h1>
-  <h3>Username</h3>
-  <input type="text" name="username_akun" id="roundedinputbox">
-  <h3>Password</h3>
-  <input type="password" name="password_akun" id="roundedinputbox">
-    <br>
-    <a href="https://www.youtube.com/watch?v=M3Keg5XKJO8"><h5>Forgot your password? Click here!<h5></a>
-
-          
-             <button type="submit" id="loginbutton">LOGIN</button> 
-          </form>
-
-          <form action="/signup" method="get">
-            <button type="submit" id="signupbutton">SIGN UP</button> 
-          </form>
+    <h1>Welcome to Digilab!</h1>
+    <h3>Username</h3>
+      <input type="text" name="username_akun" id="roundedinputbox">
+    <h3>Password</h3>
+      <input type="password" name="password_akun" id="roundedinputbox">
+      <br>
+      <a href="https://www.youtube.com/watch?v=M3Keg5XKJO8"><h5>Forgot your password? Click here!<h5></a>
+      <button type="submit" id="loginbutton">LOGIN</button> 
+  </form>
+  <form action="/signup" method="get">
+    <button type="submit" id="signupbutton">SIGN UP</button> 
+  </form>
 </body>
 </html>
